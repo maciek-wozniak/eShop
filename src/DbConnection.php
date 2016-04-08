932fa517ca39d0f8e623f68ddaa32a2cef8f04d3
Maciej Wozniak <maciek_wozniak@o2.pl>
2016-04-08 12:51:48 +0200

    db

---
+++ src/DbConnection.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'Config.php';
+
+Class DbConnection {
+
+
+    public static function getConnection() {
+        $config = new Config();
+        if ($conn = new mysqli($config->getServerName(), $config->getUserName(), $config->getPassword(), $config->getDatabase())) {
+            return $conn;
+        }
+        return false;
+    }
+}
+
+
+
+
+$conn = DbConnection::getConnection();
+
+if ($conn == false) {
+    echo 'Brak połączenia';
+}
